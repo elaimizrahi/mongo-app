aa2a723d37a8ac9f8164436a2861d1e8c2e588a9
elaimizrahi <mizrahi.elai@gmail.com>
2023-08-07 19:34:35 -0400

    update .env to protect password

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,11 @@
 import express, {Request, Response} from "express";
 import mongoose from "mongoose";
 import Deck from './models/Deck';
+import {config} from 'dotenv';
 
 const app = express();
 const port = 3000;
-
+config();
 app.use(express.json());   
 
 app.post('/decks', async (req: Request, res: Response) => {
@@ -14,7 +15,7 @@ app.post('/decks', async (req: Request, res: Response) => {
 });
 
 mongoose.connect(
-    'mongodb+srv://new_user:zYwFvRjPRXZHpMmZ@elai.jyaq15v.mongodb.net/?retryWrites=true&w=majority'
+    process.env.MONGO_URL ?? ""
     ).then(() => {
         console.log('db connection established')
         app.listen(port);
