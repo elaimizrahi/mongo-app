63073ff3980d25d224397fb7e6b1adf58e8df47c
elaimizrahi <mizrahi.elai@gmail.com>
2023-08-07 20:11:18 -0400

    create data sending to mongo db

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [title, setTitle] = useState("");
+
+  function handleCreateClick(event: React.FormEvent) {
+    event.preventDefault();
+    fetch("http://localhost:3000/decks", {
+      method: "POST",
+      body: JSON.stringify({ title: title }),
+      headers: { "Content-Type": "application/json" },
+    });
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <form onSubmit={handleCreateClick}>
+        <label htmlFor="decktitle">Deck Title</label>
+        <input
+          id="decktitle"
+          value={title}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setTitle(e.target.value);
+          }}
+        ></input>
+        <button>Create Deck</button>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,22 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
 import mongoose from "mongoose";
-import Deck from './models/Deck';
-import {config} from 'dotenv';
+import Deck from "./models/Deck";
+import { config } from "dotenv";
+import cors from "cors";
 
 const app = express();
+app.use(cors({ origin: "*" }));
 const port = 3000;
 config();
-app.use(express.json());   
+app.use(express.json());
 
-app.post('/decks', async (req: Request, res: Response) => {
-    const newDeck = new Deck({title: req.body.title});
-    const createdDeck = await newDeck.save();
-    res.json(createdDeck);
+app.post("/decks", async (req: Request, res: Response) => {
+  const newDeck = new Deck({ title: req.body.title });
+  const createdDeck = await newDeck.save();
+  res.json(createdDeck);
 });
 
-mongoose.connect(
-    process.env.MONGO_URL ?? ""
-    ).then(() => {
-        console.log('db connection established')
-        app.listen(port);
-    });
-
-
+mongoose.connect(process.env.MONGO_URL ?? "").then(() => {
+  console.log("db connection established");
+  app.listen(port);
+});
